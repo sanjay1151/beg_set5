0ecb717f3bf1b5a856120c5ce9fe1a3bff9f3030
sanjay1151 <35390831+sanjay1151@users.noreply.github.com>
2018-03-14 21:22:36 +0530

    Create 53.java

---
+++ 53.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MyClass 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner (System.in);
+        String str = sc.next();
+        String str2 = sc.next();
+        System.out.println(str+str2);
+    }
+}
